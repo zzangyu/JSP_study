5d8121fb486dc997a422155d9c3f7e95d21cbba8
zzangyu <hellohan95@gmail.com>
2022-08-08 14:36:19 +0900

    join

---
+++ ServletProject_db/src/main/java/com/join/JoinBean.java
@@ -0,0 +1,48 @@
+package com.join;
+
+public class JoinBean {
+	private String id;
+	private String pass;
+	private String name;
+	private int gender;
+	private int age;
+	private String email;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getGender() {
+		return gender;
+	}
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
